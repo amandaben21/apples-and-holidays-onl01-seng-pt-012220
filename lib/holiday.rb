44d30b0a60e133f9edc080fe5569c3d1e98c7310
Amanda Contreras <amandaben21@aol.com>
2020-02-04 03:47:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season
+holiday_hash.each do |season, h|
+  puts "#{season.capitalize}."
 
 
 
