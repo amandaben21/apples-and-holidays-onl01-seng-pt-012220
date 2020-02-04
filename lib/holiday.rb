1be161e53cd3e14a8e26b74daf1810ab7a866ddf
Amanda Contreras <amandaben21@aol.com>
2020-02-04 04:19:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ holiday_hash.each do |season, h|
   h.each do |holiday, array|
     str = holiday.to_s.split(" ")
     new_str= str.map {|w| w.capitalize}.join(" ")
-  supply_str = array.join(" , ")
+  supply_str = array.join(", ")
   puts "  #{new_str}:" " #{supply_str}"
   
   end
